d769322c07d773a66cd83de1be6ee8dff97558eb
cerg1168 <cerg1168@gmail.com>
2016-03-19 21:06:15 +0300

    Update server.py

--- server.py
+++ server.py
@@ -14,3 +14,113 @@ motor_client = motor.motor_tornado.MotorClient('localhost', 27017)
 db = motor_client['chatdb']
 init(db)
 
+
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        return self.get_cookie('name')
+
+
+class MainHandler(BaseHandler):
+    def get(self):
+        # if not self.get_current_user():
+        #     self.redirect('/loginpage')
+        f = open('static/chats/chats.html')
+        s = f.read()
+        f.close()
+        self.write(s)
+
+
+class LoginHandler(BaseHandler):
+    @coroutine
+    def post(self):
+        try:
+            data = json.loads(self.request.body.decode())
+            username = data['name']
+            password = data['password']
+        except:
+            raise
+        res = yield User.check(username, password)
+        if res:
+            self.set_cookie('name', username, expires_days=7)
+
+
+class UserHandler(BaseHandler):
+    @coroutine
+    def get(self, name):
+        user = yield User.load(name)
+        if user:
+            user = User.public_dict(user)
+            self.write(user)
+        else:
+            raise HTTPError(400, 'user doesnt exist')
+
+
+class ChangeUserHandler(BaseHandler):
+    @coroutine
+    def post(self):
+        try:
+            data = json.loads(self.request.body.decode())
+        except:
+            raise
+
+        action = data['action']
+        user_data = data['user']
+        if action == 'create':
+            if User.validate(user_data):
+                saved = yield User.save(user_data)
+                if saved:
+                    self.write('success')
+                    return
+            raise HTTPError(400, 'bad input')
+        elif action == 'update':
+            pass
+
+
+class DialogHandler(BaseHandler):
+    @coroutine
+    def post(self):
+        try:
+            data = json.loads(self.request.body.decode())
+        except:
+            raise
+        name = data['name']
+        people = data['people']
+        res = yield Dialog.create(people, name)
+        if res:
+            self.write('ok')
+        else:
+            raise HTTPError(400, 'hz')
+
+    @coroutine
+    def get(self):
+        """ :returns all dialogs of current_user """
+        user = self.get_current_user()
+        if not user:
+            raise HTTPError(300, 'forbidden')
+        res = yield Dialog.get_all_with_user(user)
+        self.write(json.dumps(res))
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        self.webSocketsPool = []
+
+        settings = {
+            'static_url_prefix': 'static/',
+            'facebook_api_key': '742008872572245',
+            'facebook_secret': 'a12c83d7452d510586921aa5d884f090',
+        }
+
+        handlers = (
+            (r'/', MainHandler),
+            (r'/static/(.*)', tornado.web.StaticFileHandler,
+            {'path': 'static/'}),
+            (r'/users/(\w*)', UserHandler),
+            (r'/users', ChangeUserHandler),
+            (r'/login', LoginHandler),
+            (r'/dialogs', DialogHandler),
+            (r'/loginpage', LoginPageHandler),
+            (r'/chats', MainHandler),
+        )
+
+        tornado.web.Application.__init__(self, handlers, **settings)
